895148c3dbd75f6fff77e196db5cc588f5e9fe2e
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-22 18:42:04 +0000

    Added a delete button to delete URLs from the list

--- express_server.js
+++ express_server.js
@@ -26,13 +26,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-}); 
-
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+}); 
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -58,8 +59,13 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {  
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {  
+  delete urlDatabase[req.params.shortURL];  
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
