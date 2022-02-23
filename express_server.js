bdc1e1f23bdcad4085b660c2e621ccf935352d95
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-23 18:11:05 +0000

    object to store data from users created

--- express_server.js
+++ express_server.js
@@ -13,6 +13,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 //function to generate a shortURL with a string of 6 random alphanumeric characters
 function generateRandomString() {
   const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
